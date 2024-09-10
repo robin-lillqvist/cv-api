6b3bf21467cd08417ea6a61af73b73f12eb52fb9
Robin Lillqvist <robin.lillqvist@knowit.se>
2024-09-10 10:48:01 +0200

    removed swagger and starting over

--- api/index.mjs
+++ api/index.mjs
@@ -1,6 +1,4 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import { swaggerJsDoc } from "./swagger.js";
 import generalInfoRoute from "./routes/generalInfo.js";
 import workPolicyRoute from "./routes/workPolicy.js";
 import techStackRoute from "./routes/techStack.js";
@@ -22,17 +20,16 @@ app.use(limiter);
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-// Serve Swagger UI at the root
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerJsDoc, { customCssUrl: CSS_URL }));
-
 // Register routes
-app.use("/api/general-info", generalInfoRoute);
-app.use("/api/work-policy", workPolicyRoute);
-app.use("/api/tech-stack", techStackRoute);
-app.use("/api/skills", skillsRoute);
-app.use("/api/job-offer", jobEnquiryRoute);
+app.use("/general-info", generalInfoRoute);
+app.use("/work-policy", workPolicyRoute);
+app.use("/tech-stack", techStackRoute);
+app.use("/skills", skillsRoute);
+app.use("/job-offer", jobEnquiryRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Node.js API!");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
